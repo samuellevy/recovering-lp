399438aa970c82d1110e8d9a5b48214ed528fb14
Samuel Levy <samuellevy@meshproducoes.com.br>
2019-07-29 20:36:25 -0300

    creating blocks on home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
-import { Container, Header, Banner } from "./styles";
+import { Container, Header, Banner, Image, Content } from "./styles";
 
 import bannerImg from "../../assets/images/banner.png";
+import logoImg from "../../assets/images/logo.png";
+import enfeiteImg from "../../assets/images/enfeite-piso.png";
 
 export default class Home extends Component {
   render() {
@@ -9,7 +11,22 @@ export default class Home extends Component {
       <Container>
         <Header>
           <Banner src={bannerImg} alt="" />
+          <Image src={logoImg} alt="" />
         </Header>
+
+        <Content>
+          <aside>
+            <h1>
+              Expertise <small>Excelência</small>
+            </h1>
+          </aside>
+
+          <aside>
+            <h1>
+              <img src={enfeiteImg} alt="" />
+            </h1>
+          </aside>
+        </Content>
       </Container>
     );
   }
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,9 +1,20 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  width: 100%;
+`;
 export const Header = styled.header`
   width: 100%;
+  position: relative;
 `;
 export const Banner = styled.img`
   width: 100%;
 `;
+
+export const Image = styled.img`
+  position: absolute;
+  top: 129px;
+  left: 14px;
+`;
+
+export const Content = styled.div``;
