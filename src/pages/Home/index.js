e2c8fd058c850d5cb63e28c34ae8a22b591f6786
Samuel Levy <samuellevy@meshproducoes.com.br>
2019-07-29 21:33:01 -0300

    banner and blocks

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Container, Header, Banner, Image, Content } from "./styles";
+import { Container, Header, Banner, Logo, Content, LeftBlock, RightBlock } from "./styles";
 
 import bannerImg from "../../assets/images/banner.png";
 import logoImg from "../../assets/images/logo.png";
@@ -11,21 +11,12 @@ export default class Home extends Component {
       <Container>
         <Header>
           <Banner src={bannerImg} alt="" />
-          <Image src={logoImg} alt="" />
+          <Logo src={logoImg} alt="" />
         </Header>
 
         <Content>
-          <aside>
-            <h1>
-              Expertise <small>Excelência</small>
-            </h1>
-          </aside>
-
-          <aside>
-            <h1>
-              <img src={enfeiteImg} alt="" />
-            </h1>
-          </aside>
+          <LeftBlock>a</LeftBlock>
+          <RightBlock>b</RightBlock>
         </Content>
       </Container>
     );
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -3,18 +3,42 @@ import styled from "styled-components";
 export const Container = styled.div`
   width: 100%;
 `;
+
 export const Header = styled.header`
-  width: 100%;
   position: relative;
+  display: flex;
+  align-items: center;
 `;
+
 export const Banner = styled.img`
   width: 100%;
+  height: 540px;
+  object-fit: cover;
+  object-position: center;
 `;
 
-export const Image = styled.img`
+export const Logo = styled.img`
   position: absolute;
-  top: 129px;
-  left: 14px;
+  left: 0;
+  margin-top: 50px;
+  margin-left: 14px;
 `;
 
-export const Content = styled.div``;
+export const Content = styled.section`
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: nowrap;
+`;
+
+export const LeftBlock = styled.div`
+  background-color: tomato;
+  height: 100px;
+  width: 50%;
+`;
+export const RightBlock = styled.div`
+  background-color: darkblue;
+  height: 100px;
+  width: 50%;
+`;
+
+export const Title = styled.h1``;
